26174660b5b2114a1f5372b55fa1a7b53184ef46
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-02-21 15:50:34 -0800

    adding a tailwind CSS form plguin and added tailwind styles in SearchParams inputs

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -42,7 +42,13 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input id="location" name="location" placeholder="Location" />
+          <input
+            type="text"
+            id="location"
+            name="location"
+            className="mb-5 block w-60"
+            placeholder="Location"
+          />
         </label>
 
         <label htmlFor="animal">
@@ -50,6 +56,7 @@ const SearchParams = () => {
           <select
             id="animal"
             name="animal"
+            className="mb-5 block w-60"
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -68,7 +75,12 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            className="mb-5 block w-60 disabled:opacity-50"
+            disabled={!breeds.length}
+            id="breed"
+            name="breed"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -78,7 +90,9 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <button>Submit</button>
+        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none bg-orange-500">
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
